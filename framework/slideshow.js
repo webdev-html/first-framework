f24fb69f3a0b6c6bbf3098a20f6914bd284c27f6
webdev-html <73722228+webdev-html@users.noreply.github.com>
2020-11-17 17:31:39 -0500

    Add files via upload

--- framework/slideshow.js
+++ framework/slideshow.js
@@ -1,5 +1,7 @@
 //you can change the code or pictures, these pictures are just for placeholders, and you can add a lot more slides to the slideshow or optimize the code
-function slide_forward1() {
+
+
+function slide_forward() {
    document.getElementById("img").src = "placeholder pictures/x.png"
    document.getElementById("arrowfor").onclick = slide_forward2
    document.getElementById("arrowback").onclick= slide_backward6
@@ -32,11 +34,11 @@ function slide_forward5() {
 
 function slide_forward6() {
     document.getElementById("img").src = "placeholder pictures/menubaricon.png"
-    document.getElementById("arrowfor").onclick = slide_forward1
-    document.getElementById("arrowback").onclick= slide_backward1
+    document.getElementById("arrowfor").onclick = slide_forward
+    document.getElementById("arrowback").onclick= slide_backward
 }
 
-function slide_backward1() {
+function slide_backward() {
     document.getElementById("img").src = "/placeholder pictures/finalframe.png"
     document.getElementById("arrowback").onclick = slide_backward2
 }
@@ -62,5 +64,33 @@ function slide_backward5() {
 
 function slide_backward6() {
     document.getElementById("img").src = "/placeholder pictures/menubaricon.png"
-    document.getElementById("arrowback").onclick = slide_backward1
+    document.getElementById("arrowback").onclick = slide_backward
+}
+
+
+function presentslideshow() {
+        
+            document.getElementById("imgbox").style.width = "100%"
+            document.getElementById("arrowback").style.width = "250px"
+            document.getElementById("arrowfor").style.width = "250px"
+            document.getElementById("present").style.backgroundColor = "yellow"
+            document.getElementById("present").style.color = "black"
+            document.getElementById("present").innerHTML = "stop presentation"
+            document.getElementById("present").style.fontSize = "150%"
+            document.getElementById("img").style.height = "152%"
+            document.getElementById("present").onclick = stopslideshow
+            
+
+        }
+
+function stopslideshow() {
+            document.getElementById("imgbox").style.width = "50%"
+            document.getElementById("arrowback").style.width = "20%"
+            document.getElementById("arrowfor").style.width = "20%"
+            document.getElementById("present").style.backgroundColor = "black"
+            document.getElementById("present").style.color = "white"
+            document.getElementById("present").innerHTML = "present"
+            document.getElementById("present").style.fontSize = "200%"
+            document.getElementById("img").style.height = "100%"
+            document.getElementById("present").onclick = presentslideshow
 }
